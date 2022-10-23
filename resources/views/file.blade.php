75b86190715160c5ccc4678ade34805b2a499535
tatsumaki0301 <kingtatumaki@gmail.com>
2022-10-23 12:06:05 +0900

    file追加

--- resources/views/file.blade.php
+++ resources/views/file.blade.php
@@ -32,35 +32,7 @@
     @csrf
     <ul>
       <li>
-        　店舗名：　<input type="text" name="shop_name">
-      </li>
-      <li>
-        　　地域：　
-          <select id="area_id" name="area_id"  class="area_name">
-            <option></option>
-            @foreach($areas as $area)
-            <option value="{{$area->id}}">
-              {{$area->area_name}}
-            </option>
-            @endforeach
-          </select>
-      </li>
-      <li>
-        ジャンル：　
-          <select id="genru_id" name="genru_id"  class="genru_name">
-            <option></option>
-            @foreach($genrus as $genru)
-            <option value="{{$genru->id}}">
-              {{$genru->genru_name}}
-            </option>
-            @endforeach
-          </select>
-      </li>
-      <li>
-        　　内容：　<textarea name="shop_content" rows="5" cols="50"></textarea>
-      </li>
-      <li>
-        　　写真：　<input type="file" name="image_path">
+        　　写真：　<input type="file" name="file_image">
       </li>
       <li>
         　　　　　　<input type="submit" value="アップロード" class="upsubmit">
@@ -70,25 +42,17 @@
 
   <div class=shop_item_area>
     <table>
-      <tr>
-        <th class="th_item">ID</th>
-        <th class="th_item">店舗名</th>
-        <th class="th_item">地域</th>
-        <th class="th_item">ジャンル</th>
-        <th>　　　　　</th>
-        <th>店舗概要</th>
+      <tr class="shop_item_title">
+        <th>ID</th>
         <th>画像</th>
       </tr>
-      @foreach ($shops as $shop)
+      @foreach ($file_images as $file_image)
       <tr>
-        <td>{{$shop->id}}</td>
-        <td>{{$shop->shop_name}}</td>
-        <td>{{$shop->area->area_name}}</td>
-        <td>{{$shop->genru->genru_name}}</td>
-        <td></td>
-        <td>{{$shop->shop_content}}</td>
         <td>
-          <img src="{{$shop->image_path}}" width="50%">
+          {{$file_image->id}}
+        </td>
+        <td>
+          <img class="shop_img" src="{{asset('storage/'.$file_image->file_image)}}" width="50%">
         </td>
       </tr>
       @endforeach
