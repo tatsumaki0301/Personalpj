9d17edb3749ec7e5f8a5033fedf9fda70d804f9a
tatsumaki0301 <kingtatumaki@gmail.com>
2022-12-25 21:34:29 +0900

    リマインダー

---
+++ app/Console/Commands/CommandTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Person;
+use App\Models\User;
+use App\Models\Shop;
+use App\Models\Reserve;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ReserveMail;
+use Carbon\Carbon;
+
+
+class CommandTest extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'laravel commandのtest';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $today = Carbon::today('Asia/Tokyo','9:00');
+        $reserves = Reserve::wheredate('datetime', $today)->with('user')->get();
+        foreach($reserves as $reserve){
+            return Mail::to($reserve->user->email)->send(new ReserveMail($reserve));
+        }
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+
     /**
      * Define the application's command schedule.
      *
@@ -16,7 +17,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('send:mail')->everyMinute();
+        $schedule->command('command:test')->everyMinute();
+        
     }
 
     /**
